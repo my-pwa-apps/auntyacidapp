1039fa5dcb7c091fc1479aae4d70aa7962bd0732
GarfieldApp <garfieldapp@outlook.com>
2026-01-04 12:38:44 +0100

    Add draggable title bar for window controls overlay mode and update cache version to v41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'auntyacid-v39';
+const CACHE_NAME = 'auntyacid-v41';
 
 // Assets to cache on install (use ./ relative paths like GarfieldApp/DirkJanApp)
 const PRECACHE_ASSETS = [
