7a6220904755a897e3df4f926dcc607faf1c440e
GarfieldApp <garfieldapp@outlook.com>
2026-01-04 12:42:35 +0100

    Update cache version to v42 and disable overscroll behavior in CSS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'auntyacid-v41';
+const CACHE_NAME = 'auntyacid-v42';
 
 // Assets to cache on install (use ./ relative paths like GarfieldApp/DirkJanApp)
 const PRECACHE_ASSETS = [
